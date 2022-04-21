b4107472b693e9d21bb9eaeaa8dcb41ca072c5ac
HAbdelwanes <HAbdelwanes@cairo.aas.com.sa>
2022-04-22 00:19:18 +0200

    add ch1 ex15

---
+++ src/main/java/ch1/Ex15.java
@@ -0,0 +1,27 @@
+package ch1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex15 {
+    static public ArrayList<ArrayList<Integer>> generatePascal(int n) {
+        var pascal = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i <= n; i++) {
+            var innerList = new ArrayList<Integer>(List.of(1));
+
+            for (int j = 1; j < i; j++) {
+                var prevList = pascal.get(i - 1);
+                innerList.add(prevList.get(j) + prevList.get(j - 1));
+            }
+
+            if (i != 0) {
+                innerList.add(1);
+            }
+
+            pascal.add(innerList);
+        }
+
+        return pascal;
+    }
+}
---
+++ src/test/java/ch1/Ex15Test.java
@@ -0,0 +1,14 @@
+package ch1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Ex15Test {
+    @Test
+    void return_expected_results() {
+        var result = Ex15.generatePascal(5);
+
+        assertEquals("[[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]", result.toString());
+    }
+}
