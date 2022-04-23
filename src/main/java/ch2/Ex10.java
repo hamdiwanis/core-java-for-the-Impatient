e4d3269adde6fdd3039f9a2f243715fe9be1e56d
HAbdelwanes <HAbdelwanes@cairo.aas.com.sa>
2022-04-23 11:49:33 +0200

    add ch2 ex10

---
+++ src/main/java/ch2/Ex10.java
@@ -0,0 +1,24 @@
+package ch2;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Ex10 {
+    public static class RandomNumbers {
+        static final Random randomGenerator = new Random(System.currentTimeMillis());
+
+        public static int randomElement(int[] list) {
+            if (list.length == 0) return 0;
+
+            int index = randomGenerator.nextInt(list.length);
+            return list[index];
+        }
+
+        public static int randomElement(ArrayList<Integer> list) {
+            if (list.isEmpty()) return 0;
+
+            int index = randomGenerator.nextInt(list.size());
+            return list.get(index);
+        }
+    }
+}
