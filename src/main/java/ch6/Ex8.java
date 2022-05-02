47a66fd3fcc3208ceb22190bbdbbc32143f09e64
HAbdelwanes <HAbdelwanes@cairo.aas.com.sa>
2022-05-02 23:53:20 +0200

    add ch6 ex8

--- src/main/java/ch6/Ex8.java
+++ src/main/java/ch6/Ex8.java
@@ -1,7 +1,7 @@
 package ch6;
 
-public class Ex7 {
-    public static class Pair<E> {
+public class Ex8 {
+    public static class Pair<E extends Comparable<? super E>> {
         private final E first, second;
 
         public Pair(E first, E second) {
@@ -15,6 +15,14 @@ public class Ex7 {
 
         public E getSecond() {
             return second;
+        }
+
+        public E max() {
+            return first.compareTo(second) < 0 ? second : first;
+        }
+
+        public E min() {
+            return first.compareTo(second) > 0 ? second : first;
         }
     }
 }
