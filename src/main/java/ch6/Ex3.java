716597abc4cb47c50d15dc49f408914b15a587e4
HAbdelwanes <HAbdelwanes@cairo.aas.com.sa>
2022-05-02 19:20:18 +0200

    add ch6 ex3

---
+++ src/main/java/ch6/Ex3.java
@@ -0,0 +1,56 @@
+package ch6;
+
+import java.util.ArrayList;
+
+public class Ex3 {
+    public static class Table<K, V> {
+        private final ArrayList<Entry<K, V>> entries = new ArrayList<>();
+
+        public V get(K key) {
+            for (var entry : entries) {
+                if (entry.getKey().equals(key)) {
+                    return entry.getValue();
+                }
+            }
+
+            return null;
+        }
+
+        public void put(K key, V value) {
+            for (var entry : entries) {
+                if (entry.getKey().equals(key)) {
+                    entry.setValue(value);
+                    return;
+                }
+            }
+
+            entries.add(new Entry(key, value));
+        }
+
+        public void remove(K key) {
+            entries.removeIf(entry -> entry.getKey().equals(key));
+        }
+    }
+
+    public static class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+}
