34ae68510979ef6feb8b0c4bd94668f68d6c41ca
HAbdelwanes <HAbdelwanes@cairo.aas.com.sa>
2022-05-03 13:52:18 +0200

    add ch7 ex7

---
+++ src/main/java/ch7/Ex7.java
@@ -0,0 +1,23 @@
+package ch7;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Ex7 {
+    public static Map<String, Integer> countWordsInfile(String filePath) {
+        String[] words = readWordsInfile(filePath);
+
+        var wordCount = new TreeMap<String, Integer>();
+
+        for (var item : words) {
+            wordCount.merge(item, 1, (oldValue, newValue) -> oldValue + 1);
+        }
+
+        return wordCount;
+    }
+
+    public static String[] readWordsInfile(String filePath) {
+        // todo: read words from file
+        return new String[]{"ABC", "ABC", "XYZ", "EFG", "XYZ", "XYZ", "HMD"};
+    }
+}
