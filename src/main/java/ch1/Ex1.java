5922d1418a7604d234319011b514f74f7342452d
HAbdelwanes <HAbdelwanes@cairo.aas.com.sa>
2022-07-08 23:48:13 +0200

    surround scanner with try resource

--- src/main/java/ch1/Ex1.java
+++ src/main/java/ch1/Ex1.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        try (Scanner in = new Scanner(System.in)) {
+            try {
+                int value = in.nextInt();
 
-        try {
-            int value = in.nextInt();
+                System.out.printf("binary: %s \n", Integer.toBinaryString(value));
+                System.out.printf("octal: %o \n", value);
+                System.out.printf("hexadecimal: %x \n", value);
 
-            System.out.printf("binary: %s \n", Integer.toBinaryString(value));
-            System.out.printf("octal: %o \n", value);
-            System.out.printf("hexadecimal: %x \n", value);
-
-            double reciprocal = 1.0 / value;
-            System.out.printf("hexadecimal floating point: %a \n", reciprocal);
-        } catch (Exception e) {
-            System.out.print("unexpected value");
+                double reciprocal = 1.0 / value;
+                System.out.printf("hexadecimal floating point: %a \n", reciprocal);
+            } catch (Exception e) {
+                System.out.print("unexpected value");
+            }
         }
     }
 }
